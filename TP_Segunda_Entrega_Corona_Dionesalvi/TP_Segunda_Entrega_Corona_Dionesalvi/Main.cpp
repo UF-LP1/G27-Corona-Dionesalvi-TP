d19585665670c55f1215fdadc6bb2dd4f816438e
isadione <isadi@LAPTOP-A6R0FKV3>
2023-05-17 16:57:39 -0300

    Mail

--- TP_Segunda_Entrega_Corona_Dionesalvi/TP_Segunda_Entrega_Corona_Dionesalvi/Main.cpp
+++ TP_Segunda_Entrega_Corona_Dionesalvi/TP_Segunda_Entrega_Corona_Dionesalvi/Main.cpp
@@ -19,17 +19,19 @@ int main()
     list <string> productos_requeridos;
     list<cEmpleado> lista_empleado; 
 
-    cFarmacia* farmacia1 = new cFarmacia("FARMALINK", "pueyrredon 5", "lunes a vienes de 8 a 21, sabados y domingos de 8 a 19", "j", ortopedia, 2, 52577, lista_productosllevados,lista_empleado ); 
+    cFarmacia* farmacia_ = new cFarmacia("FARMALINK", "pueyrredon 5", "lunes a vienes de 8 a 21, sabados y domingos de 8 a 19", "j", ortopedia, 2, 52577, lista_productosllevados,lista_empleado ); 
     cCarrito* carrito = new cCarrito(2,"chico");
     //cCarrito carr(2, "chico");
     cCliente* cliente = new cCliente("Manuel", "Morales", "44323456", "fedemorales@gmail.com", "impreso", OSDE, efectivo, "ibuprofeno", productos_requeridos, 232 , carrito , farmacia);
     cCajero* cajero = new cCajero("7821");  
+    cCajero* cajero2 = new cCajero("57497");
     cMostrador* mostrador = new cMostrador(C_10);
+    cMostrador* mostrador2 = new cMostrador(O_25);
+    cMostrador* mostrador3 = new cMostrador(G_30);
     cLimpieza* limpieza = new cLimpieza("limpieza.com", "turno tarde");
     cFarmaceutico* farmaceutico = new cFarmaceutico("5643387");
     cOrtopedista* ortopedista = new cOrtopedista("rodilla", "Sol", "Bruno", "55234543"); //falta al final la lista 
     cPerfumeros* perfumeros = new cPerfumeros("42456454", "Juan", "Carlos", "423555334");
-   
     cGolosinas* chupetin = new cGolosinas("9/04/2023", caramelo, "43242", 150, 23, "caramelo");
     cProductos_Perfumeria* productosperf = new cProductos_Perfumeria("3/03/2023", jabon, "56432", 150, 30, "Dove");
     cMedicamentos* medicamentos = new cMedicamentos("23/03/2024", "Actron", 2, "312212", 1239, 30, "Ibuprofeno");
@@ -37,16 +39,19 @@ int main()
 
     lista_productosllevados.push_front(*productomuniequera);                  //Guardo mi producto en lista de productos llevados 
 
-    float monto = cajero->cobrar(cliente,farmacia, mostrador);   //Llamamos a la funcion cobrar.
+    float monto = cajero->trabajar(cliente,farmacia_, mostrador);   //Llamamos a la funcion cobrar.
     printf("monto:%f", monto);//Imprimimos el monto total
 
     mostrador->ticket_saldopagar(cliente);
 
-    delete(farmacia);
+    delete(farmacia_);
     delete(carrito);
     delete(cliente);
     delete(cajero);
+    delete(cajero2);
     delete(mostrador);
+    delete(mostrador2);
+    delete(mostrador3);
     delete(limpieza);
     delete(farmaceutico);
     delete(ortopedista);
